cbd176a09d72215aacbc4e3d019fcebd1de8c6e7
Harryu Wang <s0930322580@gmail.com>
2021-01-18 21:59:21 +0800

    add note

---
+++ read.m
@@ -0,0 +1,3 @@
+if you have (4032,3024) mipi raw image, unpack it by mipi_unpacked.c
+after unpack mipi raw image, you can use unpacked_RAW_Cal.py to cal center ROI
+or use Select_ROI_and_save.py to to cal GRBG value and save image. 
